1ecd6db972a59b519d515be0f6f9011b51fbec75
Rizwan Ahmad Khan <ahmadkhanrizwan0@gmail.com>
2025-02-05 22:59:05 +0530

    fix deployment issue

--- src/Routes.js
+++ src/Routes.js
@@ -1,39 +1,19 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
-  useNavigate,
 } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/DashboardComponent";
 
-const ProtectedRoute = ({ children }) => {
-  const navigate = useNavigate();
-  const username = useMemo(() => localStorage.getItem("username"), []); 
-
-  useEffect(() => {
-    if (!username) {
-      navigate("/");
-    }
-  }, [username, navigate]); 
-
-  return username ? children : null;
-};
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
